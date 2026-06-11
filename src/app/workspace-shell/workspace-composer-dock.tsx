93e4b0cce84896e8047e372543ea8057b2b5b7c2
Igor Warzocha <igorwarzocha@gmail.com>
2026-06-11 09:45:48 +0100

    Align footer controls with extension status

--- src/app/workspace-shell/workspace-composer-dock.tsx
+++ src/app/workspace-shell/workspace-composer-dock.tsx
@@ -21,14 +21,21 @@ export function WorkspaceComposerDock({
     <div className="grid w-full grid-cols-[minmax(2rem,1fr)_minmax(0,800px)_minmax(2rem,1fr)] items-end gap-2 [container-type:inline-size]">
       <div className="relative z-10 col-start-2 w-full">
         {left ? (
-          <div className={cn('absolute bottom-2 left-0 z-20 min-w-0', leftClassName)}>{left}</div>
+          <div
+            className={cn(
+              'absolute bottom-[calc(0.5rem+var(--composer-extension-status-height,0px))] left-0 z-20 min-w-0',
+              leftClassName,
+            )}
+          >
+            {left}
+          </div>
         ) : null}
         {center}
       </div>
       {right ? (
         <div
           className={cn(
-            'relative z-20 col-start-3 mb-1.5 min-w-0 justify-self-start self-end',
+            'relative z-20 col-start-3 mb-[calc(0.375rem+var(--composer-extension-status-height,0px))] min-w-0 justify-self-start self-end',
             rightClassName,
           )}
         >
